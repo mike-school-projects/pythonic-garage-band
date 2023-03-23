20a83c51e4d5a4a9481a8ca54780bfed7648a9ed
Mike Shen <michael.m.shen@gmail.com>
2023-03-23 22:20:23 +0000

    initial commit

--- tests/test_pythonic_garage_band.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from pythonic_garage_band.pythonic_garage_band import Band
-
-def test_band_name():
-    actual = Band().name
-    expectation = 'test'
-    assert actual == expectation
